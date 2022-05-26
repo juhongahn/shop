74674ebbde22cd0dfe358b7a2d8879ee7500fb33
Juhong An <juhong.ahn.dev@gmail.com>
2022-05-26 20:53:05 +0900

    4장 들어가기전 커밋

---
+++ src/main/java/com/shop/dto/ItemDto.java
@@ -0,0 +1,23 @@
+package com.shop.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class ItemDto {
+
+  private Long id;
+  private String itemNm;
+  private Integer price;
+  private String itemDetail;
+  private String sellStatCd;
+  private LocalDateTime regTime;
+  private LocalDateTime updateTime;
+
+}
