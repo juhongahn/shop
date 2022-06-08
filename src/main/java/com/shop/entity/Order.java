2171db5fd617c2e96ec55b36a9d1cc50691712cd
Juhong An <juhong.ahn.dev@gmail.com>
2022-06-08 12:56:30 +0900

    7.1 주문 기능 구현하기

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -14,6 +15,9 @@ import java.util.List;
 @Entity
 @Data
 @Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Order extends BaseEntity{
 
     @Id
@@ -31,10 +35,37 @@ public class Order extends BaseEntity{
     private OrderStatus orderStatus;
 
 
-
     // 모든 엔티티를 무조건 양방향으로 매핑하면 테이블이 너무 복잡해 지므로 외래키를 가지는 테이블이
     // 먼저 단방향으로 연결하고 필요에의해 양방향으로 확장하는 방식으로 설계하자.
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL,
             orphanRemoval = true, fetch = FetchType.LAZY)
     private List<OrderItem> orderItemList = new ArrayList<>();
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItemList.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public static Order createOrder(Member member, List<OrderItem> orderItemList) {
+        Order order = new Order();
+        order.setMember(member);
+        for (OrderItem orderItem : orderItemList) {
+            // Order 객체에만 orderItem 을 넣는게 아니라 OrderItem 객체에도 order 를 넣어줘야한다.
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    public int getTotalPrice(){
+        int totalPrice = 0;
+
+        for(OrderItem orderItem : orderItemList) {
+            totalPrice +=orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
+    }
 }
---
+++ src/test/java/com/shop/service/OrderServiceTest.java
@@ -0,0 +1,81 @@
+package com.shop.service;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.dto.OrderDto;
+import com.shop.entity.Item;
+import com.shop.entity.Member;
+import com.shop.entity.Order;
+import com.shop.entity.OrderItem;
+import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.yml")
+class OrderServiceTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public Item saveItem() {
+
+        return itemRepository.save(Item.builder()
+                .itemNm("테스트 상품")
+                .price(10000)
+                .itemDetail("테스트 상품 상세 설명")
+                .itemSellStatus(ItemSellStatus.SELL)
+                .stockNumber(100)
+                .build());
+
+    }
+
+    public Member saveMember(){
+        return memberRepository.save(Member.builder()
+                .email("test@test.com")
+                .build());
+    }
+
+    @Test
+    @DisplayName("주문 테스트")
+    public void order() {
+        Item item = saveItem();
+        Member member = saveMember();
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setItemId(item.getId());
+        orderDto.setCount(10);
+
+        Long orderId = orderService.order(orderDto, member.getEmail());
+
+        Order order = orderRepository.findById(orderId).orElseThrow(EntityNotFoundException::new);
+
+        List<OrderItem> orderItemList = order.getOrderItemList();
+        int totalPrice = orderDto.getCount() * item.getPrice();
+
+        assertEquals(totalPrice, order.getTotalPrice());
+
+    }
+
+}
